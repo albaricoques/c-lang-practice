b5e424eb60af1f973962d271a9fcaca28cf308e0
alvaro <alv.axel.diaz@gmail.com>
2025-06-10 18:26:27 -0500

    solucion al ejercicio 2

--- 100ejercicios.c
+++ 100ejercicios.c
@@ -4,22 +4,33 @@
 
 int main() {
 
-//Calcular el área de un triángulo dados base y altura.
+//1. Calcular el área de un triángulo dados base y altura.
 
-    float base, height;
-    
-    printf("Insert the base: ");
-    scanf("%f", &base);
+    //float base, height;
+//    
+    //printf("Insert the base: ");
+    //scanf("%f", &base);
 
-    printf("Insert the height: ");
-    scanf("%f", &height);
+    //printf("Insert the height: ");
+    //scanf("%f", &height);
 
-    float area = (base * height) / 2;
+    //float area = (base * height) / 2;
 
-    printf("The area of your triangle is: %.2f\n", area);
+    //printf("The area of your triangle is: %.2f\n", area);
+
+//2. Convertir grados Celsius a Fahrenheit.
+
+    float celsius, fahrenheit;
+
+    printf("Insert your temperature in °C: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("Your temperature in °F is: %.1lf\n", fahrenheit);
     
     
-    
+
     
     
     
