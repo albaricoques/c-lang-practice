51d09177dd587d36dbb230849fa72023d9365379
alvaro <alv.axel.diaz@gmail.com>
2025-06-10 18:17:05 -0500

    solucion al ejercicio 1

---
+++ 100ejercicios.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+
+//Calcular el área de un triángulo dados base y altura.
+
+    float base, height;
+    
+    printf("Insert the base: ");
+    scanf("%f", &base);
+
+    printf("Insert the height: ");
+    scanf("%f", &height);
+
+    float area = (base * height) / 2;
+
+    printf("The area of your triangle is: %.2f\n", area);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
