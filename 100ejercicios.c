04d596eea3aa6fa0c6a87ae2f398ab7902ecbdb7
alvaro <alv.axel.diaz@gmail.com>
2025-06-11 21:52:43 -0500

    solucion al ejercicio 9

--- 100ejercicios.c
+++ 100ejercicios.c
@@ -161,24 +161,37 @@ int main() {
     
 //8. Leer dos números y mostrar el mayor.
 
-    int num1, num2;
-    
-    printf("Insert the first number: ");
-    scanf("%d", &num1);
+    //int num1, num2;
+//    
+    //printf("Insert the first number: ");
+    //scanf("%d", &num1);
 
-    printf("Insert the second number: ");
-    scanf("%d", &num2);
+    //printf("Insert the second number: ");
+    //scanf("%d", &num2);
 
-    if (num1 > num2)
+    //if (num1 > num2)
+    //{
+        //printf("%d\n", num1);
+    //}
+    //else if (num2 > num1)
+    //{
+        //printf("%d\n", num2);
+    //}
+    
+//9. Calcular la suma de los primeros N números naturales.
+    
+    int num;
+    int sum = 0;
+
+    printf("Insert a number: ");
+    scanf("%d", &num);
+
+    for (int i = 1; i <= num; i++)
     {
-        printf("%d\n", num1);
-    }
-    else if (num2 > num1)
-    {
-        printf("%d\n", num2);
+        sum += i;
     }
     
-    
+    printf("%d\n", sum);
     
 
    
